de2027d7d5fee221a1f670bdb648ea40352e4a1d
Henrique Reis <henriquereis198@outlook.com>
2024-08-21 10:58:15 -0300

    Criada classe com métodos para auxiliar a tratar arquivos JSON locais e adicionado arquivo com coordenadas padrão

---
+++ Weather Network/HelperUtils/JsonFileUtils.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using WeatherNetwork.WMOCodes;
+
+namespace WeatherNetwork.HelperUtils;
+
+public class JsonFileUtils
+{
+    private const string wmoCodesFile = @".\WMOCodes\WMOCodesDescription.json";
+    private const string localizationsFile = @".\DefaultLocations.json";
+
+    public static string? WMOCodeConverter(int wmoCode, string languageCode)
+    {
+        var wmoCondition = ((WMOCode)wmoCode).ToString();
+
+        using (StreamReader streamReader = new StreamReader(wmoCodesFile))
+        {
+            using (JsonDocument document = JsonDocument.Parse(streamReader.ReadToEnd()))
+            {
+                JsonElement root = document.RootElement;
+                JsonElement condition = root.GetProperty(wmoCondition);
+
+                bool descriptionExists = condition.TryGetProperty(languageCode, out JsonElement description);
+
+                if (descriptionExists)
+                    return description.ToString();
+
+                return null;
+            }
+        }
+    }
+
+    public static Dictionary<string, string>? DefaultCoordinates(string localization)
+    {
+        using (StreamReader streamReader = new StreamReader(localizationsFile))
+        {
+            using (JsonDocument document = JsonDocument.Parse(streamReader.ReadToEnd()))
+            {
+                Dictionary<string, string> coordinates = [];
+
+                JsonElement root = document.RootElement;
+                bool localizationExists = root.TryGetProperty(localization, out JsonElement foundLocalization);
+
+                if (localizationExists)
+                {
+                    coordinates.Add("latitude", foundLocalization.GetProperty("latitude").ToString());
+                    coordinates.Add("longitude", foundLocalization.GetProperty("longitude").ToString());
+
+                    return coordinates;
+                }
+
+                return null;
+                 
+            }
+        }
+    }
+}
